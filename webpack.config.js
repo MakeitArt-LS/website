391ab7f058f30775d018bc9d5e05cc80aade1530
Majd Saleh <majd.saleh95@gmail.com>
2023-11-25 20:37:54 -0800

    fixed typos causing error

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.join(_dirname, "/dist"),
+    path: path.join(__dirname, "/dist"),
     filename: "bundle.js",
   },
   plugins: [
